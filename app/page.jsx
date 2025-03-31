8942a19784cf73a1628249330af86340c99acac0
kapil9555 <kapilbehwal9555@gmail.com>
2025-03-31 11:57:48 +0530

    env files

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ export default async function Home() {
   const session = await getSession();
   const user = session?.user;
 
-  console.log(user);
+  // console.log(user);
   return (
     <div className="flex justify-center items-center h-100vh">
       
